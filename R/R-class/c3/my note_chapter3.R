9dd29a87f8f1c62bb738465813110b5b6281a3b9
WangLiuying <13656036646@163.com>
2017-03-21 08:06:00 +0800

    update notes 17/3/21

--- R/R-class/c3/my note_chapter3.R
+++ R/R-class/c3/my note_chapter3.R
@@ -12,3 +12,64 @@ leadership$agecat[leadership$age > 75] <- "Elder"
 leadership$agecat[leadership$age>=55 & leadership$age <= 75] <- "Middle Aged"
 leadership$agecat[leadership$age < 55] <- "Young"
 leadership
+
+leadership <- within(leadership,{
+  agecat <- NULL; agecat[age > 75] <- "Elder"
+  agecat[age >= 55 & age <= 75] <- "Middle Aged"
+  agecat[age < 55] <- "Young"})
+#`within` is similar with `with`,but "environment"
+
+
+#Missing Value
+x <- c(1,2,NA,3)
+sum(x)
+sum(x,na.rm = T)
+
+x <- c(1,2,NA,NaN,3)
+sum(x)
+sum(x,na.rm=T)
+
+x <- c(1,2,NA,4,NA,5)
+y <- c("a","b","c","d",NA,"f")
+good <- complete.cases(x,y)
+good
+x[good];y[good]
+
+df <- data.frame(x=x,y=y)
+na.omit(df)
+
+
+#Date Values
+x <- Sys.time()
+class(x)
+unclass(x)
+str(x)
+
+p <- as.POSIXlt(x)
+names(unclass(p))
+p$sec
+#POSIXct是个数字,POSIXlt是个列表
+
+today <- Sys.Date()
+class(today)
+as.numeric(as.Date("1970-01-01"))
+as.numeric(as.Date("1969-01-01"))
+
+format(today,format="%a %m %d %Y")
+format(today,format="%a %m %d %y")
+format(today,format="%B")
+
+dates <- c("02/27/92","02/27/92")
+class(dates)
+dates <- as.Date(dates,format = "%m/%d/%y")
+dates
+class(dates)
+unclass(dates)
+
+#perform arithmetic operations on them
+startdate <- as.Date("2004-02-13")
+enddate <- as.Date("2011-01-22")
+(days <- enddate-startdate)
+today <- Sys.Date()
+dob <- as.Date()
+difftime()
